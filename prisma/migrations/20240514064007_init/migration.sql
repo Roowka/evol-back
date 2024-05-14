fd856b16a3c6b014c2e68d72b0ee4cc05588b0b2
Lucas Goï <lucasgoi746@gmail.com>
2024-05-14 09:26:22 +0200

    first migration

---
+++ prisma/migrations/20240514064007_init/migration.sql
@@ -0,0 +1,119 @@
+-- CreateEnum
+CREATE TYPE "TypeReponse" AS ENUM ('OuiNon', 'ChoixMultiple', 'Jauge', 'ReponseLibre');
+
+-- CreateTable
+CREATE TABLE "Utilisateur" (
+    "id" SERIAL NOT NULL,
+    "nom" TEXT NOT NULL,
+    "prenom" TEXT NOT NULL,
+    "mail" TEXT NOT NULL,
+    "identifiant" TEXT NOT NULL,
+    "motdepasse" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Utilisateur_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Formation" (
+    "id" SERIAL NOT NULL,
+    "nom" TEXT NOT NULL,
+    "titrerncp" TEXT NOT NULL,
+    "description" TEXT,
+    "imageurl" TEXT,
+    "motscles" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Formation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ecole" (
+    "id" SERIAL NOT NULL,
+    "nom" TEXT NOT NULL,
+    "image" TEXT,
+    "ville" TEXT NOT NULL,
+    "departement" TEXT NOT NULL,
+    "adresse" TEXT NOT NULL,
+    "telephone" TEXT,
+    "mail" TEXT NOT NULL,
+    "identifiant" TEXT NOT NULL,
+    "motdepasse" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Ecole_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Metier" (
+    "id" SERIAL NOT NULL,
+    "nom" TEXT NOT NULL,
+    "description" TEXT,
+    "motscles" TEXT[],
+
+    CONSTRAINT "Metier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Candidature" (
+    "id" SERIAL NOT NULL,
+    "autheurId" INTEGER NOT NULL,
+    "ecoleId" INTEGER NOT NULL,
+    "texte" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Candidature_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Questionnaire" (
+    "id" SERIAL NOT NULL,
+    "utilisateurId" INTEGER NOT NULL,
+    "liste_questions_reponses" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Questionnaire_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "QuestionReponse" (
+    "id" SERIAL NOT NULL,
+    "question" TEXT NOT NULL,
+    "reponse" TEXT NOT NULL,
+    "type" "TypeReponse" NOT NULL,
+
+    CONSTRAINT "QuestionReponse_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Metier_Formation" (
+    "id" SERIAL NOT NULL,
+    "metierId" INTEGER NOT NULL,
+    "formationId" INTEGER NOT NULL,
+
+    CONSTRAINT "Metier_Formation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_mail_key" ON "Utilisateur"("mail");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_identifiant_key" ON "Utilisateur"("identifiant");
+
+-- AddForeignKey
+ALTER TABLE "Formation" ADD CONSTRAINT "Formation_id_fkey" FOREIGN KEY ("id") REFERENCES "Ecole"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Candidature" ADD CONSTRAINT "Candidature_autheurId_fkey" FOREIGN KEY ("autheurId") REFERENCES "Utilisateur"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Candidature" ADD CONSTRAINT "Candidature_ecoleId_fkey" FOREIGN KEY ("ecoleId") REFERENCES "Ecole"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Questionnaire" ADD CONSTRAINT "Questionnaire_utilisateurId_fkey" FOREIGN KEY ("utilisateurId") REFERENCES "Utilisateur"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Metier_Formation" ADD CONSTRAINT "Metier_Formation_metierId_fkey" FOREIGN KEY ("metierId") REFERENCES "Metier"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Metier_Formation" ADD CONSTRAINT "Metier_Formation_formationId_fkey" FOREIGN KEY ("formationId") REFERENCES "Formation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
